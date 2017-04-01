75637e178af06361e81c577d23b86a4ca08ebf58
tanzeelak <tnkhan8042@gmail.com>
2017-04-01 03:41:09 -0700

    buttons redirect to empty pages

--- app/src/main/java/com/uclaradio/uclaradio/MainActivity.java
+++ app/src/main/java/com/uclaradio/uclaradio/MainActivity.java
@@ -1,13 +1,57 @@
 package com.uclaradio.uclaradio;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    Button scheduleBtn;
+    Button djsBtn;
+    Button ticketsBtn;
+    Button aboutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        scheduleBtn = (Button)findViewById(R.id.scheduleBtn);
+        scheduleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Schedule.class));
+            }
+        });
+
+        djsBtn = (Button)findViewById(R.id.djsBtn);
+        djsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Djs.class));
+            }
+        });
+
+        ticketsBtn = (Button)findViewById(R.id.ticketsBtn);
+        ticketsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Tickets.class));
+            }
+        });
+
+        aboutBtn = (Button)findViewById(R.id.aboutBtn);
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, About.class));
+            }
+        });
+
+
     }
 }
