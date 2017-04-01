50fa5b9891dcc8f205ab50f71190a6c3425b7cb6
Nathan Smith <nathan@nathansmith.io>
2017-04-01 15:27:27 -0700

    Some dj stuff

--- app/src/main/java/com/uclaradio/uclaradio/Djs.java
+++ app/src/main/java/com/uclaradio/uclaradio/Djs.java
@@ -2,12 +2,49 @@ package com.uclaradio.uclaradio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import org.json.*;
 
 public class Djs extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_djs);
+
+        downloadDJs();
     }
+
+    private void downloadDJs() {
+        Ion.with(this)
+                .load("https://uclaradio.com/api/djs")
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        // data has arrived
+                        processDJs(result);
+                    }
+                });
+
+    }
+
+    private void processDJs(String result) {
+        try {
+            JSONObject json = new JSONObject(result);
+            JSONArray djs = json.getJSONArray("djs");
+            for (int i = 0; i < djs.length(); i++) {
+                JSONObject dj = djs.getJSONObject(i);
+                Log.i("tag", dj.getString("username"));
+            }
+        } catch (JSONException jsone) {
+            Log.wtf("help", jsone);
+        }
+    }
+
 }
