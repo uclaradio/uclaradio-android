f0ef3c926fc534c898c685905c4253b725e235db
tanzeelak <tnkhan8042@gmail.com>
2017-11-04 00:26:15 -0700

    sorts by day but not time

--- app/src/main/java/com/uclaradio/uclaradio/ScheduleAdapter.java
+++ app/src/main/java/com/uclaradio/uclaradio/ScheduleAdapter.java
@@ -11,6 +11,13 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Comparator;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 /**
@@ -20,8 +27,54 @@ import java.util.List;
 public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHolder> {
 
     private List<ScheduleData> items;
+    HashMap<String, Integer> dayToNum = new HashMap<String, Integer>();
+
+    public int convertDayToNum(String day){
+        return dayToNum.get(day);
+    }
 
     public ScheduleAdapter(List<ScheduleData> items) {
+
+        dayToNum.put("Sun",0);
+        dayToNum.put("Mon",1);
+        dayToNum.put("Tue",2);
+        dayToNum.put("Wed",3);
+        dayToNum.put("Thu",4);
+        dayToNum.put("Fri",5);
+        dayToNum.put("Sat",6);
+
+        Comparator<ScheduleData> dateComparator = new Comparator<ScheduleData>() {
+            @Override
+            public int compare(ScheduleData a, ScheduleData b) {
+                Integer aDay = convertDayToNum(a.getDay());
+                Integer bDay = convertDayToNum(b.getDay());
+                Integer dayComp = aDay.compareTo(bDay);
+                if (dayComp != 0){
+                    return dayComp;
+                }
+                else {
+                    SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM d HH:mm:ss zzz yyyy"); //hourAM/PM
+                    String aTime = a.getTime().toUpperCase();
+                    Log.d("aTime", aTime);
+                    String bTime = b.getTime().toUpperCase();
+                    Log.d("bTime", bTime);
+//                    Date aTimeD = sdf.parse("Thu Jun 18 20:56:02 EDT 2009");
+                    return a.getTime().compareTo(b.getTime());
+                }
+            }
+        };
+        Collections.sort(items, dateComparator);
+
+
+
+//        Collections.sort(items, new Comparator<ScheduleData>(){
+//            public int compare(ScheduleData a, ScheduleData b){
+//
+//
+//
+//                return a.getDay().compareTo(b.getDay());
+//            }
+//        });
         this.items = items;
     }
 
